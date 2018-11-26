c7021f5683cb3f719e734b63901fb37a76f6f52d
devjbence <strongerh@gmail.com>
2018-11-26 21:35:53 +0100

    db files

--- DB/tables.sql
+++ DB/tables.sql
@@ -1,3 +1,5 @@
+use szakdoga_db;
+
 CREATE TABLE `role` (
   `ID` int(11) NOT NULL AUTO_INCREMENT,
   `NAME` varchar(60) DEFAULT NULL,
@@ -28,7 +30,7 @@ CREATE TABLE `user_activation` (
   modified    TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
   expiration_date TIMESTAMP,
   PRIMARY KEY (`ID`),
-  CONSTRAINT `user_activation_for` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`)
+  FOREIGN KEY (`user_id`) REFERENCES `user` (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 CREATE TABLE `user_role` (
@@ -38,11 +40,11 @@ CREATE TABLE `user_role` (
   modified    TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
   KEY `USER_ID` (`USER_ID`),
   KEY `ROLE_ID` (`ROLE_ID`),
-  CONSTRAINT `role_user_ibfk_1` FOREIGN KEY (`ROLE_ID`) REFERENCES `role` (`id`),
-  CONSTRAINT `role_user_ibfk_2` FOREIGN KEY (`USER_ID`) REFERENCES `user` (`id`)
+  FOREIGN KEY (`ROLE_ID`) REFERENCES `role` (`id`),
+  FOREIGN KEY (`USER_ID`) REFERENCES `user` (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
-CREATE TABLE `job_offerer` ( /*with user table - one to one*/ /*with job_offerer_job_categry many to many*/
+CREATE TABLE `seller` ( 
   `ID` int(11) NOT NULL AUTO_INCREMENT,
     `user_id` int(11) not null,
   `first_name` varchar(60) not NULL,
@@ -52,11 +54,11 @@ CREATE TABLE `job_offerer` ( /*with user table - one to one*/ /*with job_offerer
    CREATED    TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
    modified    TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`ID`),
-  CONSTRAINT `user_id_foreign` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`)
+  FOREIGN KEY (`user_id`) REFERENCES `user` (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 
-CREATE TABLE `job_seeker` ( /*with user table - one to one*/ /*with job_seeker_job_categry many to many*/
+CREATE TABLE `buyer` ( /*with user table - one to one*/ /*with job_seeker_job_categry many to many*/
   `ID` int(11) NOT NULL AUTO_INCREMENT,
   `user_id` int(11) not null,
   `first_name` varchar(60) not NULL,
@@ -66,13 +68,13 @@ CREATE TABLE `job_seeker` ( /*with user table - one to one*/ /*with job_seeker_j
    CREATED    TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
    modified    TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`ID`),
-  CONSTRAINT `user_id_foreig` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`)
+  FOREIGN KEY (`user_id`) REFERENCES `user` (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 
-CREATE TABLE `job_category` ( 
+CREATE TABLE `product_category` ( 
   `ID` int(11) NOT NULL AUTO_INCREMENT,
-  `job_name` varchar(255) DEFAULT NULL,
+  `product_name` varchar(255) DEFAULT NULL,
   parent_id int(11) ,
   `about` varchar(255) DEFAULT NULL,
    active tinyint(1),
@@ -82,76 +84,44 @@ CREATE TABLE `job_category` (
 ) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 
-CREATE TABLE `job_offerer_job_category` (
-	`offerer_id` int(11) NOT NULL,
+CREATE TABLE `seller_product_category` (
+	`seller_id` int(11) NOT NULL,
 	`category_id` int(11) NOT NULL,
    CREATED    TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
    modified    TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-  CONSTRAINT `seeker_id_foreign` FOREIGN KEY (`offerer_id`) REFERENCES `job_offerer` (`id`),
-  CONSTRAINT `category_id_foreign` FOREIGN KEY (`category_id`) REFERENCES `job_category` (`id`)
+  FOREIGN KEY (`seller_id`) REFERENCES `seller` (`id`),
+  FOREIGN KEY (`category_id`) REFERENCES `product_category` (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
-CREATE TABLE `job_seeker_job_category` (
-	`seeker_id` int(11) NOT NULL,
+CREATE TABLE `buyer_job_category` (
+	`buyer_id` int(11) NOT NULL,
 	`category_id` int(11) NOT NULL,
    CREATED    TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
    modified    TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-  CONSTRAINT `seeker_id_foreig` FOREIGN KEY (`seeker_id`) REFERENCES `job_seeker` (`id`),
-  CONSTRAINT `category_id_foreig` FOREIGN KEY (`category_id`) REFERENCES `job_category` (`id`)
+  FOREIGN KEY (`buyer_id`) REFERENCES `buyer` (`id`),
+  FOREIGN KEY (`category_id`) REFERENCES `product_category` (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
-CREATE TABLE `school` (
+CREATE TABLE `product` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `seeker_id` int(11) DEFAULT NULL,
-  `from_year` int DEFAULT NULL,
-  `to_year` int DEFAULT NULL,
-  `name` varchar(200) default null,
-    CREATED    TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-  modified    TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-  PRIMARY KEY (`id`),
-  KEY `seeker_id` (`seeker_id`),
-  CONSTRAINT `job_seeker_ibfk_1` FOREIGN KEY (`seeker_id`) REFERENCES `job_seeker` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=130 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-
-CREATE TABLE `work_place` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `seeker_id` int(11) DEFAULT NULL,
-  `from_year` int DEFAULT NULL,
-  `to_year` int DEFAULT NULL,
-  `name` varchar(200) default null,
-    CREATED    TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-  modified    TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-  PRIMARY KEY (`id`),
-  KEY `seeker_id` (`seeker_id`),
-  CONSTRAINT `job_seeker_ibfk_2` FOREIGN KEY (`seeker_id`) REFERENCES `job_seeker` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=130 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-
-CREATE TABLE `job` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `offerer_id` int(11) DEFAULT NULL,
+  `seller_id` int(11) DEFAULT NULL,
   `name` varchar(200) default null,
   `description` text default null,
     CREATED    TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
   modified    TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`),
-  KEY `offerer_id` (`offerer_id`),
-  FOREIGN KEY (`offerer_id`) REFERENCES `job_offerer` (`id`)
+  KEY `seller_id` (`seller_id`),
+  FOREIGN KEY (`seller_id`) REFERENCES `seller` (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=130 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
-CREATE TABLE `job_job_category` (
-	`job_id` int(11) NOT NULL,
+CREATE TABLE `product_product_category` (
+	`product_id` int(11) NOT NULL,
 	`category_id` int(11) NOT NULL,
    CREATED    TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
    modified    TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-  FOREIGN KEY (`job_id`) REFERENCES `job` (`id`),
-  FOREIGN KEY (`category_id`) REFERENCES `job_category` (`id`)
+  FOREIGN KEY (`product_id`) REFERENCES `product` (`id`),
+  FOREIGN KEY (`category_id`) REFERENCES `product_category` (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-
-
-insert into job_category(job_name,parent_id,about,active) values('IT',null,'Információ technológia.',0);
-insert into job_category(job_name,parent_id,about,active) values('Könnyű-Fizikai',null,'Ülő/álló munka, komoly fizikai megterhelés nélkül.',0);
-insert into job_category(job_name,parent_id,about,active) values('Programozó',3,'Számítógép-programozás (vagy egyszerűen programozás) egy vagy több absztrakt algoritmus megvalósítását jelenti egy bizonyos programozási nyelven.',0);
-insert into job_category(job_name,parent_id,about,active) values('C-sharp programozó',5,'Információ technológia.',1);
 
 
 insert into role(name) values('ROLE_ADMIN');
@@ -160,33 +130,12 @@ insert into role(name) values('ROLE_USER');
 select * from role;
 select * from user;
 select * from user_role;
-select * from job_offerer;
-select * from job_seeker;
-select * from job_category;
-select * from user_activation;
-select * from job_offerer_job_category;
-select * from job_seeker_job_category;
-select * from school;
-select * from work_place;
-select * from job;
-select * from job_job_category;
 
-use rft;
 
-drop table job_offerer_job_category;
-drop table job_seeker_job_category;
-drop table job_offerer;
-drop table job_seeker;
-drop table job_category;
-drop table user_role;
-drop table user_activation;
-drop table school;
-drop table work_place;
-drop table job_job_category;
-drop table job;
+
+
 drop table user;
 
-select * from job_category;
 
 
 
